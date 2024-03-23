20b9b31ac2214835ddee2ccd6def20e56fed7fd6
patelprachipatel <prachi.patel.business@gmail.com>
2024-03-24 01:47:04 +0530

    code skeleton updated

---
+++ Xray/cloud_storage/s3_operations.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from Xray.exception import XRayException
+
+
+class S3Operaation:
+
+    def sink_folder_to_S3() ->None:
+        try:
+            pass
+        except Exception as e:
+            raise Exception(e, sys)
+
+    def sink_folder_from_S3() ->None:
+        try:
+            pass
+        except Exception as e:
+            raise Exception(e, sys) 
--- setup.py
+++ setup.py
@@ -2,11 +2,25 @@ from setuptools import find_packages,setup
 
 from typing import List
 
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    HYPEN_E_DOT="#-e."
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+        
+
+    return requirements
 
 setup(
 name="Xray",
 version="0.0.1",
 author="prachi patel",
 author_email="prachi.patel.business@gmail.com",
+install_require=get_requirements(r"C:\Users\prach\OneDrive\Desktop\ml_deployment\deep_learning_project\requirements_dev.txt"),
 package = find_packages()
 )
