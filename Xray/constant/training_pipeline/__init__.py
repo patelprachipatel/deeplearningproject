99206f9d0d767d884faafb7a9513e9fc8b6b7faa
patelprachipatel <prachi.patel.business@gmail.com>
2024-03-24 23:36:54 +0530

    config template updated

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -3,4 +3,17 @@ from typing import List
 
 import torch
 
-TIMESTAMP: datetime = datetime.now().strftime("")
+TIMESTAMP: datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+
+ARTIFACT_DIR: str="artifacts"
+
+BUCKET_NAME: str="xraylungimgs"
+
+S3_DATA_FOLDER: str="data"
+
+CLASS_LABEL_1: str="NORMAL"
+
+CLASS_LABEL_2: str="PNEUMONIA"
+
+
+
